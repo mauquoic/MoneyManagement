db3cff5274bcb243e72f4df67bd5997da5b85312
mauquoic <mauquoic@gmail.com>
2020-06-16 20:28:13 +0200

    Set default preferences
    In case the user doesn't have preferences, the default preferences will be stored for him.

--- MoneyManagementBusiness/src/main/kotlin/com/mauquoi/money/business/service/UserService.kt
+++ MoneyManagementBusiness/src/main/kotlin/com/mauquoi/money/business/service/UserService.kt
@@ -9,7 +9,14 @@ import javax.inject.Inject
 class UserService @Inject constructor(private val userRepository: UserRepository) {
 
     fun getPreferences(userId: Long): UserPreferences {
-        return userRepository.findById(userId).get().preferences?: UserPreferences()
+        val user = userRepository.findById(userId).get()
+        return if (user.preferences == null){
+            val userWithPreferences = user.copy(preferences = UserPreferences())
+            userRepository.save(userWithPreferences)
+            UserPreferences()
+        } else {
+            userRepository.findById(userId).get().preferences?: UserPreferences()
+        }
     }
 
     fun updatePreferences(userId: Long, preferences: UserPreferences) {
